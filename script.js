6fff8ac4c4cb49ad350ff8208bbdb9dd21397223
Samuel <Samuel>
2025-01-08 16:16:09 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let tablinks = document.getElementsByClassName("tab-links");
+    let tabcontents = document.getElementsByClassName("tab-contents");
+
+    function opentab(tabname) {
+      for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+      }
+      for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+      }
+      event.currentTarget.classList.add("active-link");
+      document.getElementById(tabname).classList.add("active-tab");
+    }
+    
+    let sideMenu = document.getElementById("side-menu");
+
+    function openMenu() {
+      sideMenu.style.right = "0";
+    }
+
+    function closeMenu() {
+      sideMenu.style.right = "-200px";
+    }
